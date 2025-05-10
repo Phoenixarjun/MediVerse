3739e8fd97e1a3e134c2c136cc849794c18e6ba5
Naresh <phoenixarjun007@gmail.com>
2025-05-10 15:15:40 +0530

    Folder Structure Added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+  name = "MediVerse",
+  version = "0.0.0",
+  author = "Naresh B A",
+  author_email = "phoenixarjun007@gmail.com",
+  description = "A package for Mediverse",
+  packages= find_packages(),
+  install_requires = []
+)
